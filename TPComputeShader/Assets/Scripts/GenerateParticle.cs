3ab58a2f041b766264cf6950cbdad39a7419dbde
cedricChopin <72026207+cedricChopin@users.noreply.github.com>
2022-01-18 15:06:09 +0100

    DebutProjet

---
+++ TPComputeShader/Assets/Scripts/GenerateParticle.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateParticle : MonoBehaviour
+{
+    struct Particle
+    {
+        public Vector3 position;
+        public Vector3 velocity;
+        public float life;
+    };
+}
